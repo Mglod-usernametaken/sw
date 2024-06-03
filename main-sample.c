2e1ebf894bc4dc2bae3269184dc58f504f9b7960
Mglod-usernametaken <masterwloczykij@gmail.com>
2024-06-03 21:00:06 +0200

    init LCD display shenanigans

--- main-sample.c
+++ main-sample.c
@@ -18,83 +18,129 @@
 // Use project enums instead of #define for ON and OFF.
 
 #include <xc.h>
-#include "bsp/adc.h"
-#include "bsp/buttons.h"
 #include <libpic30.h>
 
-void sleep(unsigned int time) {
-    __delay32(time * 15000);
+#define FCY     4000000UL
+#define LCD_E           LATDbits.LATD4
+#define LCD_RW          LATDbits.LATD5
+#define LCD_RS          LATBbits.LATB15
+#define LCD_DATA        LATE
+
+#define LCD_CLEAR   0x01
+#define LCD_HOME    0x02
+#define LCD_ON      0x0C
+#define LCD_OFF     0x08
+#define LCD_CONFIG  0x38
+#define LCD_CURSOR  0x80
+#define LINE1       0x00
+#define LINE2       0x40
+#define LCD_CUST_CHAR   0x40
+#define LCD_SHIFT_R     0x1D
+#define LCD_SHIFT_L     0x1B
+
+void __delay_us(unsigned long us){
+    __delay32(us*FCY/1000000);
 }
 
-int main(void) {
-    unsigned char portValue = 0x53;
-    int alarm = 0;
+void __delay_ms(unsigned long us){
+    __delay32(us*FCY/1000);
+}
 
-    ADC_SetConfiguration(ADC_CONFIGURATION_DEFAULT);
-    ADC_ChannelEnable(ADC_CHANNEL_POTENTIOMETER);
+void LCD_sendCommand(unsigned char command){
+    LCD_RW = 0;
+    LCD_RS = 0;
+    LCD_E = 1;
+    LCD_DATA = command;
+    __delay_us(50);
+    LCD_E = 0;
+}
 
-    TRISA = 0x0000;
+void LCD_sendData(unsigned char data){
+    LCD_RW = 0;
+    LCD_RS = 1;
+    LCD_E = 1;
+    LCD_DATA = data;
+    __delay_us(50);
+    LCD_E = 0;
+}
 
-    unsigned int value;
-
-    while (1) {
-        // Odczyt warto?ci potencjometru
-        value = ADC_Read10bit(ADC_CHANNEL_POTENTIOMETER);
-
-        // Sprawdzenie poprawno?ci odczytu
-        if (value == 0xFFFF) {
-            continue;
-        }
-
-        // Normalizacja warto?ci do 8 bitów
-        unsigned char normValue = value >> 2;
-
-        // Sprawdzenie progu alarmowego
-        if (normValue >= 128) {
-            alarm = 1;
-        }
-
-        // Obs?uga alarmu
-        while (alarm == 1) {
-            // Sprawdzenie przycisku do wy??czenia alarmu
-            if (BUTTON_IsPressed(BUTTON_S3) == true) {
-                alarm = 0;
-                break;
-            }
-            value = ADC_Read10bit(ADC_CHANNEL_POTENTIOMETER);
-            normValue = value >> 2;
-            if (normValue < 128){
-                alarm = 0;
-                break;
-            }
-
-            // Miganie diod? LED
-            for (int i = 0; i < 5; i++) {
-                LATA = 1;
-                sleep(100);
-                LATA = 0;
-                sleep(100);
-            }
-
-            // wlaczenie wszystkich diod
-            LATA = 255;
-
-            //wszystkie diody swieca az wylaczymy alarm
-            while(alarm = 1){
-                if(BUTTON_IsPressed(BUTTON_S3) == true){
-                    alarm =0;
-                }
-                value = ADC_Read10bit(ADC_CHANNEL_POTENTIOMETER);
-                normValue = value >> 2;
-                if (normValue < 128){
-                    alarm = 0;
-                    break;
-                }
-            }
-
-            // Wy?wietlenie warto?ci na porcie LATA
-            LATA = normValue;
-        }
+void LCD_print(unsigned char* string){
+    while(*string){
+        LCD_sendData(*string++);
     }
+}
+
+void LCD_setCursor(unsigned char row, unsigned char col){
+    unsigned char address;
+    if (row ==1){
+        address = LCD_CURSOR + LINE1 + col;
+    }
+    if (row == 2){
+        address = LCD_CURSOR + LINE2 + col;
+    }
+    LCD_sendCommand(address);
+}
+
+void LCD_saveCustChar(unsigned char slot, unsigned char *array){
+    unsigned char i;
+    LCD_sendCommand(LCD_CUST_CHAR + (slot*8));
+    for(i=0;i<8;i++){
+        LCD_sendData(array[i]);
+    }
+}
+
+void LCD_displayCharacter(unsigned char characterCode) {
+    LCD_sendData(characterCode);
+}
+
+void LCD_init(){
+    __delay_ms(20);
+    LCD_sendCommand(LCD_CONFIG);
+    __delay_us(50);
+    LCD_sendCommand(LCD_ON);
+    __delay_us(50);
+    LCD_sendCommand(LCD_ON);
+    __delay_ms(2);
+}
+
+unsigned char symbol1[8] = {
+    0b00100,
+    0b00100,
+    0b00100,
+    0b00100,
+    0b00100,
+    0b00100,
+    0b00100,
+    0b00100
+};
+
+
+
+int main(void){
+    TRISB = 0x7FFF;
+    TRISD = 0x0000;
+    TRISE = 0x0000;
+
+    LCD_init();
+    LCD_saveCustChar(0, symbol1);
+    LCD_saveCustChar(1, symbol2);
+
+
+    while(1){
+        LCD_setCursor(2,0);
+        LCD_print("Hell o, World!");
+        LCD_sendData(0);  //wyswietlenie znaku z pamieci (slot 1)
+        LCD_sendData(1);
+
+        LCD_displayCharacter(11010000);
+        __delay_ms(1000);
+        LCD_sendCommand(LCD_CLEAR);
+        __delay_ms(500);
+    }
+
     return 0;
 }
+
+
+
+
